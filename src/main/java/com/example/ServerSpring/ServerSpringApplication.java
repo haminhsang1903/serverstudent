d63c8962d1352e6f19236ac3f4818713429b38b5
haminhsang1903 <haminhsang1903@gmail.com>
2020-08-24 15:44:51 +0700

    Fix origin

--- src/main/java/com/example/ServerSpring/ServerSpringApplication.java
+++ src/main/java/com/example/ServerSpring/ServerSpringApplication.java
@@ -1,7 +1,17 @@
 package com.example.ServerSpring;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerSpringApplication {
@@ -9,5 +19,6 @@ public class ServerSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerSpringApplication.class, args);
 	}
-
+	
+	
 }
--- src/main/java/com/example/ServerSpring/controller/StudentController.java
+++ src/main/java/com/example/ServerSpring/controller/StudentController.java
@@ -10,42 +10,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ServerSpring.entity.Student;
 import com.example.ServerSpring.service.StudentService;
-@CrossOrigin(origins = "http://localhost:4200")
+
+@CrossOrigin(origins = "http://localhost:4200", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 @RestController
 public class StudentController {
 	@Autowired
 	StudentService service;
-	
-	@GetMapping({"/getAll","/"})
-	public List<Student> getAll(){
+
+	@GetMapping({ "/getAll", "/" })
+	public List<Student> getAll() {
 //		Student stu = new Student("PC00378", "Ha Minh Sang", 20);
 //		service.save(stu);
 		return service.findAll();
 	}
-	
+
 	@PostMapping("/add")
 	public void add(@RequestBody Student stu) {
 		service.save(stu);
 	}
-	
+
 	@GetMapping("/find{id}")
 	public Student findById(@PathVariable("id") String id) {
 		return service.findById(id).get();
 	}
-	
+
 	@PutMapping("/update")
 	public void update(@RequestBody Student stu) {
 		service.save(stu);
 	}
+
 	@DeleteMapping("/delete{id}")
 	public void deleteByID(@PathVariable("id") String id) {
 		service.deleteById(id);
 	}
+
 	@DeleteMapping("/delete")
 	public void deleteEntity(@RequestBody Student stu) {
 		service.delete(stu);
