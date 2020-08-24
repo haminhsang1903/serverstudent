983b28b69458d5f7c1eaf7c653d057e828200e65
haminhsang1903 <haminhsang1903@gmail.com>
2020-08-24 15:58:42 +0700

    Fix Origin

--- src/main/java/com/example/ServerSpring/WebSecurityConfig.java
+++ src/main/java/com/example/ServerSpring/WebSecurityConfig.java
@@ -1,5 +1,17 @@
 package com.example.ServerSpring;
 
-public class WebSecurityConfig {
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer{
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	            .allowedOrigins("http://localhost:4200")
+	            .allowedMethods("*")
+	            .allowedHeaders("*")
+	            .allowCredentials(false)
+	            .maxAge(3600);
+	    }
 }
--- src/main/java/com/example/ServerSpring/controller/StudentController.java
+++ src/main/java/com/example/ServerSpring/controller/StudentController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.ServerSpring.entity.Student;
 import com.example.ServerSpring.service.StudentService;
 
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "false", maxAge = 15
-		* 60, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
+//@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "false", maxAge = 15
+//		* 60, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 @RestController
 public class StudentController {
 	@Autowired
