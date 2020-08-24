f6a3d8961a5ab3b055668d5e18ecede43b0f1c2f
haminhsang1903 <haminhsang1903@gmail.com>
2020-08-24 14:56:06 +0700

    Fix CrossOrigin 4200

--- src/main/java/com/example/ServerSpring/controller/StudentController.java
+++ src/main/java/com/example/ServerSpring/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.ServerSpring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ServerSpring.entity.Student;
 import com.example.ServerSpring.service.StudentService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class StudentController {
 	@Autowired
